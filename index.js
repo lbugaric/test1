c03d176c3bc9168cb381e0f48fbb2f10a006f759
leon b19 <leon.bugaric19@gmail.com>
2023-02-13 18:53:14 +0100

    Show date in HTML

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello world");
+/**
+ * napišite javascript kod koji će u html element s id "date" upisati trenutni datum i vrijeme
+ */
+ const dateElement = document.querySelector('#date');
+ const timeElement = document.querySelector('#time');
+
+ function displayTime(){
+    const currentTime = new Date();
+    timeElement.innerHTML = `${currentTime.getHours()}h ${currentTime.getMinutes()}m ${currentTime.getSeconds()}s`;
+}
+
+function displayDate(){
+    const currentTime = new Date();
+    dateElement.innerHTML = `${currentTime}`;
+}
+
+displayTime()
+displayDate()
+
+console.log('Hello world!')
